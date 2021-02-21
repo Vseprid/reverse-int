b38aec32693344c40fd436043862a1d8ea41bc96
Vseprid <vseprid@gmail.com>
2021-02-21 15:20:51 +0200

    init: task reverse-inrt done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) n = -n;
+    let str = n.toString(10);
+    return +(str.split('').reverse().join(''));
 }
